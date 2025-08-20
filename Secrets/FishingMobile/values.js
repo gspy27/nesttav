f19a412584a0ca371c4cdf7bb355a38b4126ace7
carrot27 <165180922+gspy27@users.noreply.github.com>
2025-08-19 20:07:43 -0700

    Add files via upload

---
+++ Secrets/FishingMobile/values.js
@@ -0,0 +1,80 @@
+//Commons
+let Seaweed = 0;
+let Boot = 0;
+let Trout = 0;
+let Bass = 0;
+let Cod = 0;
+
+window.localStorage.setItem("Seaweed", Seaweed);
+window.localStorage.setItem("Boot", Boot);
+window.localStorage.setItem("Trout", Trout);
+window.localStorage.setItem("Bass", Bass);
+window.localStorage.setItem("Cod", Cod);
+
+//Uncommons
+let Walleye = 0;
+let Salmon = 0;
+let Tuna = 0;
+let Grouper = 0;
+let Mackerel = 0;
+let Halibut = 0;
+
+window.localStorage.setItem("Walleye", Walleye);
+window.localStorage.setItem("Salmon", Salmon);
+window.localStorage.setItem("Tuna", Tuna);
+window.localStorage.setItem("Grouper", Grouper);
+window.localStorage.setItem("Mackerel", Mackerel);
+window.localStorage.setItem("Halibut", Halibut);
+
+//Rares
+let Marlin = 0;
+let Snapper = 0;
+let Eel = 0;
+
+window.localStorage.setItem("Marlin", Marlin);
+window.localStorage.setItem("Snapper", Snapper);
+window.localStorage.setItem("Eel", Eel);
+
+//Really Rares
+let Shark = 0;
+let Sturgeon = 0;
+let Swordfish = 0;
+let Butterflyfish = 0;
+
+window.localStorage.setItem("Shark", Shark);
+window.localStorage.setItem("Sturgeon", Sturgeon);
+window.localStorage.setItem("Swordfish", Swordfish);
+window.localStorage.setItem("Butterflyfish", Butterflyfish);
+
+//Ultra Rares
+let Clownfish = 0;
+let Lionfish = 0;
+let Stingray = 0;
+
+window.localStorage.setItem("Clownfish", Clownfish);
+window.localStorage.setItem("Lionfish", Lionfish);
+window.localStorage.setItem("Stingray", Stingray);
+
+//Godly
+let Anglerfish = 0;
+let RedHandfish = 0;
+
+window.localStorage.setItem("Anglerfish", Anglerfish);
+window.localStorage.setItem("RedHandfish", RedHandfish);
+
+//Ungodly
+let SakhalinSturgeon = 0;
+let DevilsHolePufferfish = 0;
+
+window.localStorage.setItem("SakhalinSturgeon", SakhalinSturgeon);
+window.localStorage.setItem("DevilsHolePufferfish", DevilsHolePufferfish);
+
+//Fish
+let Fish = 0;
+
+window.localStorage.setItem("Fish", Fish);
+
+//???
+let QuestionFish = 0;
+
+window.localStorage.setItem("???", QuestionFish);
